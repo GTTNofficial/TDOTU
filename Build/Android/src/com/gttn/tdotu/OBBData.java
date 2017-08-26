5d9791948d7791a2c1a46b1250eb1a3214a546c7
Michael Gorini <michael.gorini98@gmail.com>
2017-08-26 22:33:55 +0200

    android setup

---
+++ Build/Android/src/com/gttn/tdotu/OBBData.java
@@ -0,0 +1,24 @@
+package com.gttn.tdotu;
+
+public class OBBData
+{
+public static class XAPKFile {
+public final boolean mIsMain;
+public final String mFileVersion;
+public final long mFileSize;
+XAPKFile(boolean isMain, String fileVersion, long fileSize) {
+mIsMain = isMain;
+mFileVersion = fileVersion;
+mFileSize = fileSize;
+}
+}
+
+public static final XAPKFile[] xAPKS = {
+new XAPKFile(
+true, // true signifies a main file
+"1", // the version of the APK that the file was uploaded against
+283320059L // the length of the file in bytes
+)
+};
+};
+
