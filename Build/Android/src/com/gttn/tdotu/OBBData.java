7dd4b8857cbe955f29676f929f61f2d68dd60ed9
Michael Gorini <michael.gorini98@gmail.com>
2017-08-27 13:04:49 +0200

    Ottimizzazione xd parte 2

--- Build/Android/src/com/gttn/tdotu/OBBData.java
+++ Build/Android/src/com/gttn/tdotu/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-283320059L // the length of the file in bytes
+32946976L // the length of the file in bytes
 )
 };
 };
