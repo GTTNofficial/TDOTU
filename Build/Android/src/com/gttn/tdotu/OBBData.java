024f76cbd40b35563a022dfc7e19f67009e7f803
Matteo Ungaro <unconfigured@null.spigotmc.org>
2017-09-03 09:38:33 +0200

    l

--- Build/Android/src/com/gttn/tdotu/OBBData.java
+++ Build/Android/src/com/gttn/tdotu/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-17106025L // the length of the file in bytes
+22996265L // the length of the file in bytes
 )
 };
 };
